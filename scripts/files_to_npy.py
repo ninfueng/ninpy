343e5cc349f6fea43a85b4b6028fc79366b23ea2
ninnart-fuengfusin <ninnart.fuengfusin@yahoo.com>
2021-04-20 14:44:33 +0900

    Update lastest

--- scripts/files_to_npy.py
+++ scripts/files_to_npy.py
@@ -1,15 +1,25 @@
+"""Save images into npy format.
+"""
 import glob
 import os
 import sys
 
 import imageio
 import numpy as np
+from tqdm import tqdm
+
+# TODO: argparse instead of argv!!!
 
 if __name__ == "__main__":
-    print("moving images in", sys.argv[1], "to", sys.argv[2])
+    print("Move images in", sys.argv[1], "to", sys.argv[2])
+
     files = glob.glob(os.path.join(sys.argv[1], "*.png"))
     shape = imageio.imread(files[0]).shape
     data = np.zeros(shape=(len(files), *shape), dtype=np.uint8)
-    for idx, f in enumerate(files):
+
+    pbar = tqdm(files)
+    pbar.set_description("Move images to npy.")
+    for idx, f in enumerate(pbar):
         data[idx] = imageio.imread(f)
+
     np.save(sys.argv[2], data)
